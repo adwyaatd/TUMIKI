8486732701c92fecddb0dd14dc8c6f61152be05e
細田来夢 <52363465+adwyaatd@users.noreply.github.com>
2020-01-15 18:18:34 +0900

    sqliteからmysqlへsqlを変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,55 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191110081915) do
-
-  create_table "goals", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "goal"
-    t.string   "purpose"
-    t.date     "date"
-    t.integer  "users_id"
-    t.string   "user_name"
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-  end
-
-  create_table "rules", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.string   "user_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "image_name"
-    t.string   "password_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
