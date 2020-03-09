c895116e5e8f499f810c88451f9e712d7d6a35d3
細田来夢 <52363465+adwyaatd@users.noreply.github.com>
2020-03-09 12:31:40 +0900

    redirect renderのパスを修正 user_topの部分テンプレート追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     member do
       get :likes
       get :goals
+      get :tasks
     end
 
     collection do
@@ -38,9 +39,6 @@ Rails.application.routes.draw do
     end
   end
 
- post "/likes/:post_id/create" => "likes#create"
- post "/likes/:post_id/destroy" => "likes#destroy"
-
   resources :tasks do
     member do
       post :done
@@ -49,33 +47,4 @@ Rails.application.routes.draw do
   end
 
   resources :rules
-
-#   get "/posts/index" => "posts#index"
-#   get "/posts/:id/new" => "posts#new"
-#   post "/posts/:id/create" => "posts#create"
-#   get "/posts/:id" => "posts#show"
-#   get "/posts/:id/edit" => "posts#edit"
-#  post "/posts/:id/update" => "posts#update"
-#  post "/posts/:id/destroy" => "posts#destroy"
-
-#  get "/goals/:id/set_goal" => "goal#set_goal"
-#  post "/goals/:id/create_goal"=>"goal#create_goal"
-#  get "/goals/:id/edit_goal" => "goal#edit_goal"
-#  post "/goals/:id/update" => "goal#update"
-#  get "/goals/:id/record" => "goal#record"
-#  post "/goals/:id/done" => "goal#done"
-#  post "/goals/:id/unfinish" => "goal#unfinish"
-#  get "/goals/:id/reset" => "goal#reset"
-#  post "/goals/:id/recreate" => "goal#recreate"
-
-#  get "/tasks/:id/new" => "tasks#new"
-#  get "/tasks/:id/edit" => "tasks#edit"
-#  post "/tasks/:id/create" => "tasks#create"
-#  post "/tasks/:id/destroy" => "tasks#destroy"
-#  post "/tasks/:id/done" => "tasks#done"
-#  post "/tasks/:id/unfinish" => "tasks#unfinish"
-#  get "/tasks/:id" => "tasks#index"
-#  post "/tasks/:id/update" => "tasks#update"
-
-
 end
