cc3447ead2f5199e3a454366a1bbab69c01112a2
細田来夢 <52363465+adwyaatd@users.noreply.github.com>
2020-03-13 12:21:42 +0900

    Seleniumを導入

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,40 +1,49 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.feature "Posts", type: :feature do
-  scenario"user creates a new post"do
-    #ユーザーを作成
-    user = User.create(
-      name:"鈴木一郎",
-      email:"test@gmail.com",
-      password:"ichiro"
-    )
-    #目標を作成
-    goal = Goal.create(
-      goal:"testgoal",
-      purpose:"testpurpose",
-      date:"1111-11-11",
-      user_id:1,
-      users_name:"鈴木一郎",
-    )
 
-    #お試し投稿を作成
-    post = Post.create(
-      content:"テスト投稿",
-      user_id:1
-    )
+  feature "表示部分" do
+    before do
+      visit root_path
+    end
 
-    #トップページへアクセス
-    visit root_path
+    scenario"user creates a new post"do
+      #トップページが表示される" do
+      expect(current_path).to eq "/"
+      expect(page).to have_content "TUMIKI"
 
-    # ログイン画面へ遷移
-    # click_on "ログイン"
+      #ユーザーを作成
+      user = User.create(
+        name:"鈴木一郎",
+        email:"test@gmail.com",
+        password:"ichiro"
+      )
+      #目標を作成
+      goal = Goal.create(
+        goal:"testgoal",
+        purpose:"testpurpose",
+        date:"1111-11-11",
+        user_id:1,
+        users_name:"鈴木一郎",
+      )
 
-    #アドレスとパスワードを入力してログイン
-    fill_in "email", with: user.email
-    fill_in "password", with: user.password
-    click_button"ログイン"
+      #お試し投稿を作成
+      post = Post.create(
+        content:"テスト投稿",
+        user_id:1
+      )
 
-    #ログイン成功のフラッシュメッセージが表示される
-    expect(page).to have_content "ログインしました"
+      # ログイン画面へ遷移
+      click_on "ログイン"
+
+      # #アドレスとパスワードを入力してログイン
+      # fill_in "email", with: user.email
+      # fill_in "password", with: user.password
+      # click_button"ログイン"
+
+      # #ログイン成功のフラッシュメッセージが表示される
+      # expect(page).to have_content "ログインしました"
+    end
   end
 end
